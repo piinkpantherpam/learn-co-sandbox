47f83b80a2dae0b378dca9ab67b0389cf3bd9f28
Pam Cairo <pcairo_90@outlook.com>
2020-10-27 19:38:28 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,9 @@
+answer = "< fill in your answer here >"
+puts "A group of flamingos is called a #{answer}."
+
+answer = "< fill in your answer here >"
+puts "A group of flamingos is called a #{answer}."
+answer = "flamboyance"
+
+answer = "flamboyance"
+puts "A group of flamingos is called a #{answer}."
